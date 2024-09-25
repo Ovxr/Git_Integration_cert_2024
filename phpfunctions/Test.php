333bec4d1a7caecc85c82b973bd214269bb96dc8
Tudor Ovidiu <162560675+Ovxr@users.noreply.github.com>
2024-09-25 10:34:57 +0300

    Update Test.php

--- phpfunctions/Test.php
+++ phpfunctions/Test.php
@@ -4,7 +4,7 @@ class className extends JobRouter\Engine\Runtime\PhpFunction\DialogFunction
 {
 	public function execute($rowId = null)
 	{
-		// Insert Functionality here
+		$this -> getTableValue('test');
 	}
 }
 ?>
